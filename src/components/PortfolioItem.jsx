d1709d02a0681843b46b02939a051cdc639fdb0f
daly-brendan <91297091+daly-brendan@users.noreply.github.com>
2022-12-04 15:26:00 -0500

    Update PortfolioItem.jsx

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -10,7 +10,7 @@ function PortfolioItem({title, imgUrl, stack}) {
             />
             <div className = "w-full p-4">
                 <h3 className = "text-large md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">{title}</h3>
-                <p className = "flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                <p className = "flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
                     {stack.map(item => (
                         <span className = "inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
                             {item}
