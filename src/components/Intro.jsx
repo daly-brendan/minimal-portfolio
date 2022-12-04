8fdd877d1f9d709ed2a6f16edbd3e4a3ea7bcbeb
daly-brendan <91297091+daly-brendan@users.noreply.github.com>
2022-12-04 14:52:18 -0500

    Update Intro.jsx
    Added Bio to intro section.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,8 @@ function Intro() {
         <div className = "flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className ="text-4xl md:text-7xl dark: text-white mb-1 md:md-3 font-bold">Brendan</h1>
             <p className ="text-base md:text-xl md-3 font-medium">Software engineer and web developer</p>
-            <p className ="text-sm max-w-xl bm-6 font-bold">Bio</p>
+            <p className ="text-sm max-w-xl bm-6 font-bold">Hi! My name is Brendan Daly. I am a freelance web and fullstack developer. I am currently finising
+            my masters degree in data science from Montclair State University. See below for some of the cool things I have worked on and some of the project I am currently working on.</p>
         </div>
     )
 }
